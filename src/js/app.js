1df39377ff27701a98b1eae4b43e7a4cc4ed0d61
flatlinediver <flatlinediver@protonmail.com>
2021-03-24 22:07:49 -0300

    fixed wrong version of spinner

--- src/js/app.js
+++ src/js/app.js
@@ -4,18 +4,18 @@ import { Wrapper } from './appStyle';
 import { styleTheme } from './utils'
 import { Main, Sidebar } from './containers';
 import { useStore } from './store';
-import {ReactSpinnerProvider} from '@flatlinediver/react-spinner'
+import {SpinnerProvider} from '@flatlinediver/react-spinner'
 
 export default () => {
     const {store} = useStore();
 	return (
 		<ThemeProvider theme={styleTheme}>
-			<ReactSpinnerProvider value={store}>
+			<SpinnerProvider value={store}>
 				<Wrapper>
 					<Sidebar />
 					<Main />
 				</Wrapper>
-			</ReactSpinnerProvider>
+			</SpinnerProvider>
 		</ThemeProvider>
 	)
 }
